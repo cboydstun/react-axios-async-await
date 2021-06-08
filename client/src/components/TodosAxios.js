2ec9472588c80a6a5984fac5938165fec53fc42f
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-06-08 08:40:22 -0500

    axios cleanup

--- client/src/components/TodosAxios.js
+++ client/src/components/TodosAxios.js
@@ -5,7 +5,7 @@ export default function TodosAxios() {
   const [todos, setTodos] = useState([])
   useEffect(() => {
     axios.get("https://jsonplaceholder.typicode.com/todos")
-      .then( (res) => {
+      .then((res) => {
           console.log(res.data)
           setTodos(res.data)
       })
@@ -13,13 +13,15 @@ export default function TodosAxios() {
 
   return (
     <div>
-      {todos.map((todo) => {
-        return (
-          <div key={todo.id}>
-            <h1>Todo Title: {todo.title}</h1>
-          </div>
-        )
-      })}
+      {
+        todos.map((todo) => {
+            return (
+            <div key={todo.id}>
+                <h1>Todo Title: {todo.title}</h1>
+            </div>
+            )
+        })
+      }
     </div>
   )
 }
