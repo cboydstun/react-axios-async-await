f90756f53265ff3f4a7bbcead87d68904e7dbbb5
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-06-08 08:51:51 -0500

    added async component

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ export default function App() {
     return (
         <div>
             {/* <TodosFetch /> */}
-            <TodosAxios />
+            {/* <TodosAxios /> */}
             <TodosAsync />
         </div>
     )
--- client/src/components/TodosAsync.js
+++ client/src/components/TodosAsync.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 export default function TodosAsync() {
-    return (
-        <div>
-            hey from async
-        </div>
-    )
+
+  const [todos, setTodos] = useState([])
+
+  useEffect(async () => {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/todos")
+    let data = res.data;
+    setTodos(data)
+  }, [])
+
+  return (
+    <div>
+      {todos.map((todo) => {
+        return (
+          <div key={todo.id}>
+            <h1>Todo Title: {todo.title}</h1>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
