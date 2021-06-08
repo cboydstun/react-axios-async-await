46914d1319df4f0006abbcf8d4a7702c0c94e9ea
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-06-08 08:38:59 -0500

    added axios

--- client/src/components/TodosAxios.js
+++ client/src/components/TodosAxios.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
 export default function TodosAxios() {
-    return (
-        <div>
-            hey from axios
-        </div>
-    )
+  const [todos, setTodos] = useState([])
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/todos")
+      .then( (res) => {
+          console.log(res.data)
+          setTodos(res.data)
+      })
+  }, [])
+
+  return (
+    <div>
+      {todos.map((todo) => {
+        return (
+          <div key={todo.id}>
+            <h1>Todo Title: {todo.title}</h1>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
